d543e03179ffaf9c079772c4205b48ac83f30984
Serzhio <Makhov.Serezha2015@gmail.com>
2023-08-17 17:35:31 +0300

    fixed the logic of working with the dialog mode

--- handlers/search_companies_handler.py
+++ handlers/search_companies_handler.py
@@ -5,6 +5,7 @@ from aiogram.filters import Command
 from keyboards.choose_category_kb import choose_category_keyboard
 from aiogram.fsm.state import State, StatesGroup
 
+
 text_choose_category_incorrectly = 'Категорию можно выбрать только из предложенного списка ниже'
 array_of_category = ['Развитие проекта', 'Подборки', 'Продвижение', 'Образование и наука', 'Финансы', 'Полезные материалы']
 
@@ -20,23 +21,26 @@ async def cmd_search_companies(message: Message, state: FSMContext):
     await state.set_state(SearchCompaniesProcess.choose_category_process)
 
 @router.message(
-    SearchCompaniesProcess.give_free_answer_process,
-    F.text
-)
-async def cmd_answer(message: Message, state: FSMContext):
-    await message.answer(f"Я принял ваш запрос: {message.text}\n\nСейчас я пришлю список, подходящих вам компаний")
-    await state.clear()
-
-@router.message(
     SearchCompaniesProcess.choose_category_process,
     F.text.in_(array_of_category)
 )
 async def cmd_choose_category(message: Message, state: FSMContext):
     await message.answer("Отлично, теперь опишите подробнее ваш запрос в свободной форме.", reply_markup=types.ReplyKeyboardRemove())
+    await state.update_data(selected_category = message.text.lower()) # сохранили информацию
     await state.set_state(SearchCompaniesProcess.give_free_answer_process)
 
 @router.message(SearchCompaniesProcess.choose_category_process)
 async def cmd_choose_category_incorrectly(message: Message):
     await message.answer(text=text_choose_category_incorrectly, reply_markup=choose_category_keyboard)
+
+@router.message(
+    SearchCompaniesProcess.give_free_answer_process,
+    F.text
+)
+async def cmd_answer(message: Message, state: FSMContext):
+    await state.update_data(detailed_request= message.text.lower())
+    user_data = await state.get_data()
+    await message.answer(f"<b>Ваш запрос принят!</b>\n\n<b>Категория:</b> {user_data['selected_category']}\n\n<b>Подробный запрос:</b> {user_data['detailed_request']}\n\nСейчас я пришлю список карточек, подходящих вам компаний", parse_mode='HTML')
+    await state.clear()
 
 
